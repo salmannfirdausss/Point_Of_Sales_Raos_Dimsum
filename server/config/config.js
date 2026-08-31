0aedb17701b7c5cde6572265097023a4003a7d6b
JAYY <salmannfirdauss@gmail.com>
2026-08-31 22:12:03 +0700

    add Features: Get detail saus, Validation pages, Navigate pages, Logout button, Token, Add login limitter

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -11,12 +11,15 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + '_test',
+    database: process.env.DB_NAME + "_test",
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
 };
